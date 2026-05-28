e66b50f268dbabb262e9eb35c62c5d99667694bc
capt-alien <ericbotcher@gmail.com>
2026-05-27 20:35:43 -0700

    added ECR repos

--- infra/aws/main.tf
+++ infra/aws/main.tf
@@ -1,3 +1,19 @@
 data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
+
+resource "aws_ecr_repository" "go_service" {
+  name = "${var.project_name}-go"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "swift_service" {
+  name = "${var.project_name}-swift"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
--- infra/aws/outputs.tf
+++ infra/aws/outputs.tf
@@ -5,3 +5,11 @@ output "aws_account_id" {
 output "aws_region" {
   value = data.aws_region.current.name
 }
+
+output "go_ecr_repository_url" {
+  value = aws_ecr_repository.go_service.repository_url
+}
+
+output "swift_ecr_repository_url" {
+  value = aws_ecr_repository.swift_service.repository_url
+}
