d4cb38449a5c6a072d53f648a3e6d05bbf8832cc
capt-alien <ericbotcher@gmail.com>
2026-06-11 18:39:07 -0700

    added external DNS for svc

--- infra/gcp/cloudflare.tf
+++ infra/gcp/cloudflare.tf
@@ -6,3 +6,12 @@ resource "cloudflare_record" "gcp_stateless_app" {
   ttl     = 60
   proxied = false
 }
+
+resource "cloudflare_record" "gcp_grafana" {
+  zone_id = var.cloudflare_zone_id
+  name    = "grafana"
+  type    = "A"
+  content = var.gcp_lb_ip
+  ttl     = 60
+  proxied = false
+}
